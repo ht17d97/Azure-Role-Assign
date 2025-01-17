4e36d8600ea839395f02d044c26b0c43ac7bd70b
ht17d97 <63954538+ht17d97@users.noreply.github.com>
2025-01-17 21:22:33 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+data "azurerm_subscription" "primary" {
+
+}
+
+data "azurerm_client_config" "example" {
+}
+
+resource "azurerm_role_assignment" "example" {
+  scope                = data.azurerm_subscription.primary.id
+  role_definition_name = "Contributor"
+  principal_id         = data.azurerm_client_config.example.object_id
+}
