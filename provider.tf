793460c5b5710105716006d1a7f6aa4070ee4988
ht17d97 <63954538+ht17d97@users.noreply.github.com>
2025-01-19 12:01:56 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,27 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.16.0"
+    }
+    azuread = {
+      source = "hashicorp/azuread"
+      version = "3.1.0"
+    }
+  }
+}
+
 provider "azurerm" {
   subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
   client_id       = var.client_id
   client_secret   = var.client_secret
-  features {}
+  features {
+  }
+}
+
+provider "azuread" {
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
 }
