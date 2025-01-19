4bb7ac08e8a89c4d16a34f946a4bde36777a4090
ht17d97 <63954538+ht17d97@users.noreply.github.com>
2025-01-19 11:19:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,11 @@
 data "azurerm_subscription" "primary" {
-
 }
 
 data "azurerm_client_config" "example" {
+}
+
+data "azuread_user" "example_user" {
+  user_principal_name = "John@himanshutariyal2025outlook.onmicrosoft.com"
 }
 
 resource "azurerm_role_assignment" "example" {
